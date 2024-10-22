58ce8da8b8f2374b0e235170a54f4519f36cf167
JayJunor <Mvojunior237@gmail.com>
2024-10-22 22:03:17 +0200

    final version for exercise B01

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -6,35 +6,36 @@ export class Name {
     private components: string[] = [];
     private delimiter: string = this.DEFAULT_DELIMITER;
 
+    // @methodtype initialization method
     constructor(other: string[], delimiter?: string) {
         this.components = other;
         this.delimiter = delimiter?.valueOf() ?? this.DEFAULT_DELIMITER;
     }
-
+    // @methodtype conversion method
     public asNameString(delimiter: string = this.delimiter): string {
         return this.components.join(delimiter);
     }
-
+    // @methodtype getter
     public getComponent(i: number): string {
         return this.components[i];
     }
-
+    // @methodtype setter
     public setComponent(i: number, c: string): void {
         this.components[i] = c;
     }
-
+    // @methodtype getter
     public getNoComponents(): number {
         return this.components.length;
     }
-
+    // @methodtype command
     public insert(i: number, c: string): void {
         this.components.splice(i,0,c);
     }
-
+    // @methodtype command
     public append(c: string): void {
         this.components.push(c);
     }
-
+    // @methodtype command method
     public remove(i: number): void {
         this.components.splice(i,1);
     }
